5ba6ce6b9ac79807965e8a9b268b12af2dbc7fb5
Yahekoo <zafin.dev@gmail.com>
2023-09-24 23:39:24 +0100

    Commit-4 : Finalisation de l'Authentication Provider

--- src/main/java/com/security/demo/services/AuthProvider.java
+++ src/main/java/com/security/demo/services/AuthProvider.java
@@ -6,7 +6,9 @@ import com.security.demo.repositories.AttemptsRepository;
 import com.security.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.LockedException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -31,13 +33,25 @@ public class AuthProvider implements AuthenticationProvider {
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String username = authentication.getName();
-        Optional<Attempts> opt_attempts = attemptsRepository.findAttemptsByUsername(username);
-        if(opt_attempts.isPresent()) {
-            Attempts attempts = opt_attempts.get();
-            attempts.setAttempts(0);
-            attemptsRepository.save(attempts);
+        User user = (User) userDetailsService.loadUserByUsername(username);
+        boolean is_password_match = false;
+        if(!user.isAccountNonLocked()) {
+            throw new LockedException("Account Locked Due To Many Attempts");
+        } else {
+            is_password_match = passwordEncoder.matches(authentication.getCredentials().toString(),user.getPassword());
         }
-        return authentication;
+       if(is_password_match) {
+          Optional<Attempts> opt_attempts = attemptsRepository.findAttemptsByUsername(username);
+           if(opt_attempts.isPresent()) {
+               Attempts attempts = opt_attempts.get();
+               attempts.setAttempts(0);
+               attemptsRepository.save(attempts);
+           }
+           return new UsernamePasswordAuthenticationToken(authentication.getPrincipal(),authentication.getCredentials());
+       } else {
+           processFailedAttempts(username,user);
+           throw new AuthenticationServiceException("Authentication Failed");
+       }
     }
 
     private void processFailedAttempts(String username, User user) throws AuthenticationException {
